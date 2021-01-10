c0b51c648e0b65fc9f8322065bbab4bbbc02642d
OceanT-shirt <stylish.trousers@gmail.com>
2021-01-10 10:07:00 +0900

    modified venv

---
+++ DjangoProject/diary/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views import generic
+from .forms import InquiryForm
+
+
+# Create your views here.
+class IndexView(generic.TemplateView):
+    template_name = 'diary/index.html'
+
+
+class InquiryView(generic.FormView):
+    template_name = 'diary/inquiry.html'
+    form_class = InquiryForm
